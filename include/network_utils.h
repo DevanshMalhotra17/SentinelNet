b6250dfebdd8eb56698980b088e43f09509c4615
DevanshMalhotra17 <devanshmalhotra17@gmail.com>
2025-12-22 23:09:31 -0500

    Add IP range and CIDR parsing utilities

--- include/network_utils.h
+++ include/network_utils.h
@@ -9,4 +9,8 @@ public:
     static uint32_t ipToInt(const std::string& ip);
     
     static std::string intToIp(uint32_t ip);
+
+    static std::vector<std::string> expandCIDR(const std::string& cidr);
+
+    static std::vector<std::string> expandRange(const std::string& range);
 };
--- src/network_utils.cpp
+++ src/network_utils.cpp
@@ -28,3 +28,55 @@ std::string NetworkUtils::intToIp(uint32_t ip) {
        << (ip & 0xFF);
     return ss.str();
 }
+
+std::vector<std::string> NetworkUtils::expandCIDR(const std::string& cidr) {
+    std::vector<std::string> ips;
+    
+    size_t slashPos = cidr.find('/');
+    if (slashPos == std::string::npos) {
+        throw std::invalid_argument("Invalid CIDR notation");
+    }
+    
+    std::string baseIp = cidr.substr(0, slashPos);
+    int prefixLength = std::stoi(cidr.substr(slashPos + 1));
+    
+    if (prefixLength < 0 || prefixLength > 32) {
+        throw std::invalid_argument("Invalid prefix length");
+    }
+    
+    uint32_t base = ipToInt(baseIp);
+    uint32_t mask = (0xFFFFFFFF << (32 - prefixLength)) & 0xFFFFFFFF;
+    uint32_t network = base & mask;
+    uint32_t hostMask = ~mask;
+    
+    for (uint32_t host = 1; host < hostMask; host++) {
+        ips.push_back(intToIp(network | host));
+    }
+    
+    return ips;
+}
+
+std::vector<std::string> NetworkUtils::expandRange(const std::string& range) {
+    std::vector<std::string> ips;
+    
+    size_t dashPos = range.find('-');
+    if (dashPos == std::string::npos) {
+        throw std::invalid_argument("Invalid range format");
+    }
+    
+    std::string startIp = range.substr(0, dashPos);
+    std::string endIp = range.substr(dashPos + 1);
+    
+    uint32_t start = ipToInt(startIp);
+    uint32_t end = ipToInt(endIp);
+    
+    if (start > end) {
+        throw std::invalid_argument("Start IP must be less than end IP");
+    }
+    
+    for (uint32_t ip = start; ip <= end; ip++) {
+        ips.push_back(intToIp(ip));
+    }
+    
+    return ips;
+}
