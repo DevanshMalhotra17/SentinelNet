4e0c0118ecb1a187e219a07064b454c04420d0e1
DevanshMalhotra17 <devanshmalhotra17@gmail.com>
2026-01-22 20:25:46 -0500

    Add multi-threaded network discovery

--- include/scanner.h
+++ include/scanner.h
@@ -22,13 +22,11 @@ public:
   std::vector<NetworkInterface> getInterfaces() const;
 
   std::string scan() const;
+  std::vector<int> scanPorts(const std::string &target, const std::vector<int> &ports) const;
 
-  std::vector<int> scanPorts(const std::string &target,
-                             const std::vector<int> &ports) const;
-
-  AuditResult grabBanner(const std::string &ip, int port) const;
-
-  bool isHostAlive(const std::string &target, int timeoutMs = 500) const;
+  bool isHostAlive(const std::string &target, int timeoutMs = 1000) const;
 
   std::vector<std::string> getArpHosts() const;
+  std::vector<std::string> discoverActiveHosts(const std::string& subnet) const;
+  AuditResult grabBanner(const std::string &ip, int port) const;
 };
